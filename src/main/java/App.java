eceb64f3359460b8a3d87e93b0bf4237c14817fc
Joseph Cameron <jf.cameron@gmail.com>
2018-03-22 18:08:26 -0400

    wip

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,9 @@
 // 
 // 
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Program entry object
  */
@@ -21,5 +24,14 @@ public class App
     public static void main(String[] args)
     {
         System.out.println(new App().getGreeting());
+
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
+
+        //String path = App.getBasePathForClass(App.class);
+        //String applicationPath=  new File(path + "application.jar").getAbsolutePath();
+
+        //System.out.println(path);
     }
 }
